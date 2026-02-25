19af249d46b20c2f1a1595b4df9b69ba7faf69ce
AldoFlota18 <aldoflota11@gmail.com>
2026-02-25 00:30:14 -0600

    Se agregaron los botones para visualizar las contraseñas y se cambio el titulo de la pantalla principal a ANTHIVE STOCK

--- obj/Debug/net10.0-ios/iossimulator-x64/Invex_App.AssemblyInfo.cs
+++ obj/Debug/net10.0-ios/iossimulator-x64/Invex_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Invex_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+02045a5284b990cb9339f3a997b08fef61c0cb80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a6afe74d2fa9ef7aec8409de1bf96023281c5865")]
 [assembly: System.Reflection.AssemblyProductAttribute("Invex_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Invex_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
