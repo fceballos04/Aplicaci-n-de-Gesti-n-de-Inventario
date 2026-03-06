594e46467513960b66fd9cd4ecc0561e2cff469b
aldoflota11@gmail.com <aldoflota11@gmail.com>
2026-03-06 11:47:11 -0600

    Se agrego el modo oscuro y claro corregido y se modifico el subtitulo

--- obj/Debug/net10.0-windows10.0.19041.0/win-x64/Invex_App.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows10.0.19041.0/win-x64/Invex_App.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Invex_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e58e13cdc81d60d44622f06811da2e3ede58ac83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7aef6e32e0f35a8fde465e81c21e1a38502a0573")]
 [assembly: System.Reflection.AssemblyProductAttribute("Invex_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Invex_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
